9df531021e26b7c5a4f525a9097a351a10216712
DiginessForever <diginess@gmail.com>
2017-09-14 19:18:50 -0500

    Shows today's and tomorrow's random movies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,11 +9,16 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    tomorrowsMovie = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
     content += "<ul>"
     content += "<li>" + movie + "</li>"
+    content += "</ul>"
+    content += "<h1>Tomorrow's Movie:</h1>"
+    content += "<ul>"
+    content += "<li>" + tomorrowsMovie + "</li>"
     content += "</ul>"
 
     # TODO: pick another random movie, and display it under
@@ -23,7 +29,15 @@ def index():
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie_list = [
+        "The Big Lebowski",
+        "Fallen",
+        "Gattica",
+        "It",
+        "Star Wars Episode V"
+    ]
 
+    #return movie_list[1]
+    return movie_list[random.randrange(len(movie_list))]
 
 app.run()
